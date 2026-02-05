e444005291f484aaff16d3b2e45972d8100b39af
TC Brown <trrncbrown@gmail.com>
2026-02-05 14:52:58 -0600

    refactor: add `post-header` class to blog title and rename blog navigation button variable.

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
-var button = document.getElementById("post-1");
+/* Template for button navigation of Blog Tile */
+var blogButton = document.getElementById("post-1");
 
-if (button) {
-    button.addEventListener("click", function () {
+if (blogButton) {
+    blogButton.addEventListener("click", function () {
         window.location.href = "post/post-1.html";
     });
 }
