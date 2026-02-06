cd719b13df2e7e47ee9409870e3a0a113b29d017
TC Brown <trrncbrown@gmail.com>
2026-02-06 06:13:12 -0600

    Updated: Style for links on home page to buttons for hub navigation on index.html

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,35 @@ if (blogButton) {
         window.location.href = "post/post-1.html";
     });
 }
+
+var blogButton = document.getElementById("blog-link");
+
+if (blogButton) {
+    blogButton.addEventListener('click', function () {
+        window.location.href = "temp/blog/blog.html"
+    });
+}
+
+var crockpotButton = document.getElementById("cp-link");
+
+if (crockpotButton) {
+    crockpotButton.addEventListener("click", function () {
+        window.location.href = "temp/crockpot/crockpot.html";
+    });
+}
+
+var recipesButton = document.getElementById("recipes-link");
+
+if (recipesButton) {
+    recipesButton.addEventListener("click", function () {
+        window.location.href = "temp/recipes/recipes.html"
+    });
+}
+
+var storeButton = document.getElementById("store-link");
+
+if(storeButton) {
+    storeButton.addEventListener("click", function () {
+        window.location.href = "temp/store/store.html"
+    });
+}
